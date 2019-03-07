Fixed errors in the dataset as well as adding rotation. Annealed learning rate for the sccnn.

combine_all_binary.py
@@ -17,37 +17,31 @@ class_count = defaultdict(int)
 dirs_train = []
 dirs_test = []
 
-# train_dict = defaultdict(list)
-# test_dict = defaultdict(list)
-train_list = []
-test_list = []
+train_dict = defaultdict(list)
+test_dict = defaultdict(list)
 
+others_counter = 0
 counter = 0
 test_counter = 0
-multiplier = 3
 for j, d in enumerate(dirs):
     filename = d+'_{}.mat'
     mats = [scipy.io.loadmat(os.path.join(root_dir,d,filename.format(m)))['detection'] for m in mat_names]
     for i,m in enumerate(mats):
         for item in m:
-            l = [d, item, classes[mat_names[i]]]
+            l = [d, item, 0 if i<2 else 1]
+            if l[2] == 0:
+                others_counter += 1
             if j < 80:
                 class_count[i] += 1
                 class_count['total'] += 1
-                # train_dict[counter] = l
-                for _ in range(multiplier):
-                    train_list.append(l)
+                train_dict[counter] = l
                 counter += 1
             else:
-                # test_dict[test_counter] = l
-                test_list.append(l)
+                test_dict[test_counter] = l
                 test_counter += 1
 
-np.random.shuffle(train_list)
-np.random.shuffle(test_list)
 
-# np.save(os.path.join(root_dir,'train_dict.npy'), train_dict)
-# np.save(os.path.join(root_dir,'test_dict.npy'), test_dict)
-np.save(os.path.join(root_dir,'train_list.npy'), train_list)
-np.save(os.path.join(root_dir,'test_list.npy'), test_list)
+np.save(os.path.join(root_dir,'train_dict.npy'), train_dict)
+np.save(os.path.join(root_dir,'test_dict.npy'), test_dict)
 np.save(os.path.join(root_dir,'class_weights.npy'), class_count)
+# print(others_counter)

data.py
@@ -8,7 +8,8 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 from PIL import Image
-from combine_class import read_dict
+from combine_all import read_dict
+from mark_centers import mark_cen
 
 class ClassificationDataset(Dataset):
     """ Dataset containing histological tissue scans """
@@ -16,22 +17,26 @@ class ClassificationDataset(Dataset):
     def __init__(self, root_dir, train=True, shift=None, transform=None):
         self.root_dir = root_dir
         self.train = train
-        self.items = np.load(os.path.join(root_dir,'{}_dict.npy'.format('train' if self.train else 'test'))).item()
+        self.items = np.load(os.path.join(root_dir,'{}_list.npy'.format('train' if self.train else 'test')))  #.item()
         # self.dirs = np.array([[d]*7 for d in os.listdir(self.root_dir) if os.path.isdir(os.path.join(self.root_dir, d))]).flatten()
         self.transform = transform
         self.shift = shift
         self.width = 27
         self.height = 27
+        self.rotations = [0, 90, 180, 270]
         _mean = [0.485, 0.456, 0.406]
         _std = [0.229, 0.224, 0.225]
         if not self.transform:
             self.transform = transforms.Compose([
                 transforms.RandomHorizontalFlip(),
                 transforms.RandomVerticalFlip(),
+                transforms.ColorJitter(hue=0.05, saturation=0.1, brightness=0.1),
                 transforms.ToTensor(),
                 transforms.Normalize(_mean, _std)
             ])
         self.offset = np.array([13,13])
+        self.coarse_crop = 41
+        self.coarse_offset = np.array([(self.coarse_crop-1)/2]*2)
 
     def __len__(self):
         return len(self.items)
@@ -42,17 +47,30 @@ class ClassificationDataset(Dataset):
         dir_name = item[0]
         img_name = os.path.join(self.root_dir, dir_name,'{}.bmp'.format(dir_name))
         img_pil = Image.open(img_name)
+        # mark_cen(img_pil,[item])
+        img_pil = transforms.Pad(int((self.coarse_crop-1)/2))(img_pil)
+        # Rotate image randomly from self.rotations degrees
+        # img_pil = transforms.functional.rotate(img_pil, np.random.choice(self.rotations))
         
         center = item[1].astype(float)
         if self.shift:
             center += np.random.uniform(0-self.shift,self.shift, size=(2,)).astype(float)
-        center = np.round_(np.array(center)).astype(int)
+        center = np.round_(np.array(center)).astype(int)+self.coarse_offset
 
-        left_top = np.maximum.reduce([center - self.offset, np.zeros(2)])
-        cropped_img = transforms.functional.crop(img_pil,*left_top[::-1], self.width, self.height)
+        # left_top = np.maximum.reduce([center - self.offset, np.zeros(2)])
+        # cropped_img = transforms.functional.crop(img_pil,*left_top[::-1], self.width, self.height)
 
-        if self.transform:
+        coarse_left_top = center-self.coarse_offset
+        cropped_img = transforms.functional.crop(img_pil,*coarse_left_top[::-1],self.coarse_crop,self.coarse_crop)
+        cropped_img = transforms.functional.rotate(cropped_img, np.random.choice(self.rotations))
+        coarse_center = (np.array(cropped_img.size)-1)/2
+        left_top = coarse_center-self.offset
+        cropped_img = transforms.functional.crop(cropped_img, *left_top[::-1], self.width, self.height)
+
+        if self.transform and self.train:
             net_input = self.transform(cropped_img)
+        else:
+            net_input = transforms.ToTensor()(cropped_img)
 
         target = torch.tensor(item[2], dtype=torch.long)
 

ignite_softmax_sccnn.py
@@ -6,7 +6,7 @@ import numpy as np
 import os
 
 from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
-from ignite.metrics import Accuracy, Loss
+from ignite.metrics import Accuracy, Loss, Precision, Recall
 from ignite.handlers import ModelCheckpoint
 from ignite.contrib.handlers.param_scheduler import LRScheduler
 from torch.optim.lr_scheduler import MultiStepLR
@@ -23,8 +23,10 @@ class softmaxSCCNN(nn.Module):
         # Globals
         self.num_classes = num_classes
         self.p = dropout_p
+        self.class_weights = loss_weights
         if not criterion:
-            self.criterion = nn.CrossEntropyLoss(weight=loss_weights)
+            # self.criterion = nn.CrossEntropyLoss(weight=loss_weights)
+            self.criterion = nn.NLLLoss(weight=self.class_weights)
 
 
         # Layers
@@ -33,54 +35,6 @@ class softmaxSCCNN(nn.Module):
         self.fc1 = nn.Linear(5*5*48,512)
         self.fc2 = nn.Linear(512,512)
         self.fc3 = nn.Linear(512,self.num_classes)
-
-
-    def save_model(self, filename='model/{}.pth', all=False):
-        """
-        Saves the model. If _all_ is true then it will save the whole model, otherwise only its parameters.
-        """
-        if all:
-            torch.save(self, filename.format('model'))
-        else:
-            torch.save(self.state_dict(), filename.format('statedict'))
-
-    def load_model(self, filename, all=False):
-        """
-        Loads a model. If _all_ is true then it will load a whole model, otherwise only its parameters.
-        Puts the model into evaluation mode.
-        """
-        if all:
-            self = torch.load(filename.format('model'))
-        else:
-            self.load_state_dict(torch.load(filename))
-        self.eval()
-
-    def save_checkpoint(self, epoch, optimizer, criterion ,filename='checkpoints/model_{}.tar'):
-        """
-        Saves the model along with the current training parameters for further training.
-        Good to save regularly during training in case of failure or if manual tweaking is wanted.
-        """
-        from datetime import datetime
-
-        current_datetime = datetime.now().strftime('%Y-%m-%d_%H_%M')
-        torch.save({
-            'epoch': epoch,
-            'model_state_dict': self.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss_criterion': criterion
-            }, filename.format(current_datetime))
-
-    def load_checkpoint(self, filename):
-        """
-        Returns the loaded checkpoint dictionary detailed in the save_checkpoint method
-        and sets the model parameters and loss criterion to those of the checkpoint.
-        The return value can be used to continue training, as inputs to the train_model method.
-        """
-        checkpoint_dict = torch.load(filename)
-        self.load_state_dict(checkpoint_dict['model_state_dict'])
-        self.criterion = checkpoint_dict['loss_criterion']
-        return checkpoint_dict
-
 
     def forward(self, X):
         """
@@ -95,8 +49,9 @@ class softmaxSCCNN(nn.Module):
         y = F.dropout(y, p=self.p)
         y = F.relu(self.fc2(y))
         y = F.dropout(y, p=self.p)
-        # y = F.softmax(self.fc3(y), dim=1)
-        y = self.fc3(y)
+        y = F.log_softmax(self.fc3(y), dim=1)
+        
+        # y = self.fc3(y)
 
         return y
 
@@ -108,9 +63,6 @@ class softmaxSCCNN(nn.Module):
         out = F.softmax(self.forward(data), dim=1)
         out_choices = torch.argmax(out, 1)
         correct_percentage = torch.sum(torch.where(target==out_choices,torch.tensor(1.),torch.tensor(0.)))/len(target)
-        if np.random.rand() < 0.05 and False:
-            print(target)
-            print(out_choices)
         return correct_percentage
     
 
@@ -122,21 +74,47 @@ class softmaxSCCNN(nn.Module):
         if init:
             self.apply(init_weights)
         trainer = create_supervised_trainer(self, optimizer, self.criterion)
-        evaluator = create_supervised_evaluator(self, metrics={'accuracy': Accuracy(), 'loss':Loss(self.criterion)})
+        evaluator = create_supervised_evaluator(self, 
+            metrics={'accuracy': Accuracy(), 'precision': Precision(), 'recall': Recall(), 'loss': Loss(self.criterion)})
 
         step_scheduler = MultiStepLR(optimizer, milestones=[60, 100], gamma=0.1)
         scheduler = LRScheduler(step_scheduler)
 
+        # Print out metrics with some defined interval (if statement)
         @trainer.on(Events.EPOCH_COMPLETED)
         def validate(trainer):
-            if trainer.state.iteration % 1 == 0:
+            if trainer.state.epoch % 1 == 0:
                 evaluator.run(val_loader)
                 metrics = evaluator.state.metrics
-                print("After {} epochs, accuracy = {:.4f}, loss = {:.6f}".format(trainer.state.epoch, metrics['accuracy'], metrics['loss']))
+                F1 = (2*metrics['precision']*metrics['recall']/(metrics['precision']+metrics['recall'])).numpy()
+                F1[np.isnan(F1)] = 0
+                print(
+                    ("After {} epochs, Accuracy = {:.4f}, Loss = {:.6f}\n\t Prec\t={}\n\t Recall\t={}\n\t"+ \
+                        "F1\t={}\n\t Weighted Average F1 score: {}\n")
+                    .format(trainer.state.epoch, 
+                        metrics['accuracy'], 
+                        metrics['loss'], 
+                        metrics['precision'].numpy(), 
+                        metrics['recall'].numpy(),
+                        F1,
+                        np.sum(F1*self.class_weights.numpy())))
+        
+        # Check to see if the learning rate scheduler is working correctly
+        # @trainer.on(Events.EPOCH_COMPLETED)
+        def check_grads(trainer):
+            check = True
+            if trainer.state.epoch % 20 == 0 or trainer.state.epoch % 20 == 1:
+                for g in optimizer.param_groups:
+                    check = check and g['lr'] == 0.01
+                print('Learning rate check at epoch {} start is {}'.format(
+                trainer.state.epoch,
+                check
+            ))
+
         
         checkpointer = ModelCheckpoint('checkpoints', 'ignite', save_interval=10, create_dir=True, require_empty=False)
         trainer.add_event_handler(Events.EPOCH_COMPLETED, checkpointer, {'model': self})
-        # trainer.add_event_handler(Events.EPOCH_STARTED, scheduler)
+        trainer.add_event_handler(Events.EPOCH_STARTED, scheduler)
 
         print("Start training!")
         trainer.run(train_loader, max_epochs=max_epochs)
@@ -149,7 +127,7 @@ def change_lr(optim, new_lr):
 
 def init_weights(m):
     if type(m) == nn.Linear or type(m) == nn.Conv2d:
-        torch.nn.init.normal_(m.weight, std=0.01)
+        torch.nn.init.normal_(m.weight, std=0.02)
         m.bias.data.fill_(0.0)
 
 def init_weights_linear(m):
@@ -160,23 +138,25 @@ def init_weights_linear(m):
 
     
 if __name__ == "__main__":
+    num_classes = 4
+    batch_size = 100
+    num_workers = 4
     root_dir = '/Users/gudjonragnar/Documents/KTH/Thesis/CRCHistoPhenotypes_2016_04_28/Classification'
-    train_ds = ClassificationDataset(root_dir=root_dir, train=True, shift=4.)
-    train_dl = DataLoader(train_ds, batch_size=32, shuffle=True, num_workers=4)
+    train_ds = ClassificationDataset(root_dir=root_dir, train=True)
+    train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
     test_ds = ClassificationDataset(root_dir=root_dir, train=False)
-    test_dl = DataLoader(test_ds, batch_size=32, num_workers=4)
+    test_dl = DataLoader(test_ds, batch_size=batch_size, num_workers=num_workers)
 
     class_weight_dict = np.load(os.path.join(root_dir,'class_weights.npy')).item()
-    class_weights = torch.tensor([class_weight_dict[i]/class_weight_dict['total'] for i in range(4)], dtype=torch.float)
-    net = softmaxSCCNN(loss_weights=class_weights)
+    class_weights = torch.tensor([class_weight_dict[i]/class_weight_dict['total'] for i in range(num_classes)], dtype=torch.float)
+    net = softmaxSCCNN(loss_weights=class_weights, num_classes=num_classes)
 
     # data = torch.rand(3,3,27,27)
 
-    # optimizer = torch.optim.Adam(net.parameters(), lr=0.1, weight_decay=5e-4)
+    # optimizer = torch.optim.Adam(net.parameters(), lr=10)
     # optimizer = torch.optim.Adam(net.parameters(), lr=1, weight_decay=5e-4)
     optimizer = torch.optim.SGD(net.parameters(), lr=0.01, weight_decay=5e-4, momentum=0.9)
-
 
     def t(n=3):
         net.train_model(train_dl, optimizer, max_epochs=n, val_loader=test_dl)

mark_centers.py
@@ -2,19 +2,25 @@ import numpy as np
 from PIL import Image
 import os
 from torchvision.transforms.functional import crop
+import scipy.io
 
 root_dir='/Users/gudjonragnar/Documents/KTH/Thesis/CRCHistoPhenotypes_2016_04_28/Classification'
 
 til = Image.new('RGB',(2,2), (250,0,0))
 
 def load(img_name):
+    mat_names = ['others', 'fibroblast', 'epithelial', 'inflammatory']
+    mats = [scipy.io.loadmat(os.path.join(root_dir,img_name,(img_name+"_{}").format(m)))['detection'] for m in mat_names]
     img = Image.open(os.path.join(root_dir,img_name,img_name+'.bmp'))
-    centers = np.load(os.path.join(root_dir,img_name,img_name+'.npy')).item()
+    centers = []
+    for m in mats:
+        for item in m:
+            centers.append(['d',item,1])
     return img, centers
 
-def mark_centers(img,centers):
-    for c, cla in centers.items():
-        c_tuple = tuple(np.round_(c,decimals=0).astype(int))
+def mark_cen(img,centers):
+    for c in centers:
+        c_tuple = tuple(np.round_(c[1],decimals=0).astype(int))
         img.paste(til, c_tuple)
 
 def crop_(img, center, offset=np.array([13,13]), size=[27,27]):
@@ -24,15 +30,12 @@ def crop_(img, center, offset=np.array([13,13]), size=[27,27]):
     cropped_2 = crop(img,*left_top[::-1],*size)
     return cropped_1, cropped_2
 
-img, centers = load('img8')
-mark_centers(img, centers)
-# img.show()
+if __name__ == '__main__':
+        img, centers = load('img8')
+        mark_cen(img, centers)
+        img.show()
 
-for key, val in centers.items():
-    c = key
-    break
 
-c1, c2 = crop_(img, c)
 
 
 