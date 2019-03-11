fe8aee6b89a7f6f3002c1a1d36d25571242dcf84
Guðjón <grbr@kth.se>
2019-03-11 16:28:51 +0100

    Added NEP dataset and testing for NEP accuracy. Moved criterion out ofthe model to make loading easier.

---
+++ acc_test.py
@@ -0,0 +1,51 @@
+import torch
+import params
+
+from ignite_softmax_sccnn import softmaxSCCNN
+from data import NEPValidationDataset, ClassificationDataset
+from ignite.engine import create_supervised_evaluator
+from ignite.metrics import Accuracy
+from torch.utils.data import DataLoader
+
+def handler(output):
+    pred, target = output
+    pred = torch.sum(pred, dim=0)
+    stack = [pred]*len(target)
+    return torch.stack(stack), target
+
+model = softmaxSCCNN()
+nep_evaluator = create_supervised_evaluator(model, metrics={'acc': Accuracy(handler)})
+evaluator = create_supervised_evaluator(model, metrics={'acc': Accuracy()})
+
+nep_test_ds = NEPValidationDataset(root_dir=params.root_dir, d=3)
+nep_test_dl = DataLoader(nep_test_ds, batch_size=len(nep_test_ds.shifts), num_workers=params.num_workers, drop_last=True)
+
+test_ds = ClassificationDataset(root_dir=params.root_dir, train=False)
+test_dl = DataLoader(test_ds, batch_size=params.batch_size, num_workers=params.num_workers)
+
+# it = iter(test_dl)
+# print(next(it)[1])
+
+test_output = torch.tensor([
+    [.1, .1, .5, .3],
+    [.1, .1, .5, .3],
+    [.1, .1, .5, .3]
+])
+test_target = torch.tensor([1,1,1])
+
+# print(handler((test_output,test_target)))
+evaluator.run(test_dl)
+nep_evaluator.run(nep_test_dl)
+metrics = evaluator.state.metrics
+nep_metrics = nep_evaluator.state.metrics
+print('Single patch accuracy: {}, NEP accuracy: {}'.format(metrics['acc'], nep_metrics['acc']))
+
+print('Loading trained model!')
+model.load_model('checkpoints/sccnn_model_5.pth')
+print('Loading finished!')
+
+evaluator.run(test_dl)
+nep_evaluator.run(nep_test_dl)
+metrics = evaluator.state.metrics
+nep_metrics = nep_evaluator.state.metrics
+print('Single patch accuracy: {}, NEP accuracy: {}'.format(metrics['acc'], nep_metrics['acc']))
--- data.py
+++ data.py
@@ -35,38 +35,34 @@ class ClassificationDataset(Dataset):
                 # transforms.Normalize(_mean, _std)
             ])
         self.offset = np.array([13,13])
-        self.coarse_crop = 41
-        self.coarse_offset = np.array([(self.coarse_crop-1)/2]*2)
+        self.pad = 20
 
     def __len__(self):
         return len(self.items)
 
-    def __getitem__(self, idx):
-        # Item has structure [dir_name, coords, class] where dir_name is like 'img3'
-        item = self.items[idx]
+    def load_img(self, item):
         dir_name = item[0]
         img_name = os.path.join(self.root_dir, dir_name,'{}.bmp'.format(dir_name))
         img_pil = Image.open(img_name)
         # mark_cen(img_pil,[item])
-        img_pil = transforms.Pad(int((self.coarse_crop-1)/2))(img_pil)
-        # Rotate image randomly from self.rotations degrees
-        # img_pil = transforms.functional.rotate(img_pil, np.random.choice(self.rotations))
+        img_pil = transforms.Pad(self.pad)(img_pil)
+        return img_pil
+
+    def __getitem__(self, idx):
+        # Item has structure [dir_name, coords, class] where dir_name is like 'img3'
+        item = self.items[idx]
+        img_pil = self.load_img(item)
         
         center = item[1].astype(float)
         if self.shift:
             center += np.random.uniform(0-self.shift,self.shift, size=(2,)).astype(float)
-        center = np.round_(np.array(center)).astype(int) + int((self.coarse_crop-1)/2) 
+        center = np.round_(np.array(center)).astype(int) + self.pad 
 
         # left_top = np.maximum.reduce([center - self.offset, np.zeros(2)])
         left_top = center-self.offset
         cropped_img = transforms.functional.crop(img_pil,*left_top[::-1], self.width, self.height)
 
-        # coarse_left_top = center-self.coarse_offset
-        # cropped_img = transforms.functional.crop(img_pil,*coarse_left_top[::-1],self.coarse_crop,self.coarse_crop)
         cropped_img = transforms.functional.rotate(cropped_img, np.random.choice(self.rotations))
-        # coarse_center = (np.array(cropped_img.size)-1)/2
-        # left_top = coarse_center-self.offset
-        # cropped_img = transforms.functional.crop(cropped_img, *left_top[::-1], self.width, self.height)
 
         if self.transform and self.train:
             net_input = self.transform(cropped_img)
@@ -76,6 +72,41 @@ class ClassificationDataset(Dataset):
         target = torch.tensor(item[2], dtype=torch.long)
 
         return (net_input, target)
+
+
+class NEPValidationDataset(ClassificationDataset):
+    
+    def __init__(self, root_dir, d=2):
+        super().__init__(root_dir, train=False)
+        self.d = d
+        self.shifts = [np.array([i,j]) for i in range(-self.d,self.d+1) for j in range(-self.d,self.d+1)]
+    
+    def __len__(self):
+        return len(self.shifts)*len(self.items)
+    
+    def __getitem__(self, idx):
+        _idx = idx//len(self.shifts)
+        _idx_mod = idx % len(self.shifts)
+        item = self.items[_idx]
+        img_pil = self.load_img(item)
+
+        center = item[1].astype(float) + self.shifts[_idx_mod] + self.pad
+        
+        left_top = np.round_(center-self.offset)
+        cropped_img = transforms.functional.crop(img_pil,*left_top[::-1], self.width, self.height)
+        if cropped_img.size[0] != self.width or cropped_img.size[1] != self.height:
+            print("Dimensions of crop is wrong! The dimensions are {},\n\tthe center is {} and left_top is {}."
+                .format(cropped_img.size, center, left_top))
+        
+        net_input = transforms.ToTensor()(cropped_img)
+        target = torch.tensor(item[2], dtype=torch.long)
+
+        return net_input, target
+
+
+
+
+
 
 if __name__ == '__main__':
     ds = ClassificationDataset(root_dir=params.root_dir)
